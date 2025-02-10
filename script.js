94858eb6740d2ce556e5054f0ad3032ed40b777c
Virginie <virginie.gay94@gmail.com>
2025-02-10 10:03:34 +0100

    maj-script

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // ----Animation des écrits page compétence-----
 
 let txtHtml = document.querySelector(".txt-html");
+let txtcss = document.querySelector(".txt-css");
+let txtjs = document.querySelector(".txt-js");
 let visible = true;
 
 setInterval(() => {
     visible = !visible;
     txtHtml.style.opacity = visible ? "1" : "0";
+    txtcss.style.opacity = visible ? "1" : "0";
+    txtjs.style.opacity = visible ? "1" : "0";
 }, 1000)
